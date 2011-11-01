bb334c777a30584eee8ec7241a2f4e32fff6233a
chrisfarms <chrisfarms@gmail.com>
2011-11-01 22:18:03 +0000

    multipart yenc deocder

---
+++ yenc_test.go
@@ -0,0 +1,33 @@
+package yenc
+
+import(
+    "testing"
+    "os"
+)
+
+func TestSinglepartDecode(t *testing.T) {
+    f,err := os.Open("singlepart_test.yenc")
+    if err != nil {
+        t.Fatal("could not open singlepart_test.yenc for testing")
+    }
+    _,err = Decode(f)
+    if err != nil {
+        t.Fatal("expected to decode: " + err.String())
+    }
+}
+
+func TestMultipartDecode(t *testing.T) {
+    f,err := os.Open("multipart_test.yenc")
+    if err != nil {
+        t.Fatal("could not open multipart_test.yenc for testing")
+    }
+    part,err := Decode(f)
+    if err != nil {
+        t.Errorf("expected to decode: " + err.String())
+    }
+    if part.Name != "joystick.jpg" {
+        t.Errorf("expected part name %s got %s", "joystick.jpg", part.Name)
+    }
+    // out,_ := os.Create("joystick.jpg")
+    // out.Write(part.Body)
+}
